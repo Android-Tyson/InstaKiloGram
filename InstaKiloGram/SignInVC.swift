613046eca957babdc04f8eaa03fb7213ab1a63e3
Android-Tyson <lim.tyson132@gmail.com>
2017-08-28 20:44:42 +0545

    Facebook auth Completed

--- InstaKiloGram/SignInVC.swift
+++ InstaKiloGram/SignInVC.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil{
+                print("Tyson: Unable to authenticate with facebook \(error!)")
+            }else if result?.isCancelled == true{
+                print("Tyson: User cancelled facebook")
+            }else{
+                print("Tyson: User successfully authenticated.")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential){
+        Auth.auth().signIn(with: credential, completion: {(user,error) in
+            if error != nil{
+                print("Tyson: Unable to authenticate with facebook \(error!)")
+            }else {
+                print("Tyson: Successfully authenticated facebook")
+            }
+        })
+    }
 }
+
+
+
+
+
+
+
 
