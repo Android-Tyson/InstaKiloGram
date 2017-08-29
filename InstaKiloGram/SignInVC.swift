5122146111ac1c8f243f386a164d2893893306e3
Android-Tyson <lim.tyson132@gmail.com>
2017-08-29 18:38:13 +0545

    Completed creating Firebase DB users

--- InstaKiloGram/SignInVC.swift
+++ InstaKiloGram/SignInVC.swift
@@ -50,7 +50,8 @@ class SignInVC: UIViewController {
             }else {
                 print("Tyson: Successfully authenticated facebook")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider":credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         })
@@ -62,7 +63,8 @@ class SignInVC: UIViewController {
                 if error == nil{
                     print("Tyson: Email user authenticate with firebase")
                     if let user = user{
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider":user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 }else{
                     Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
@@ -71,7 +73,8 @@ class SignInVC: UIViewController {
                         }else{
                             print("Tyson: successfylly authenticated with firebase using email")
                             if let user = user{
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider":user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     })
@@ -80,7 +83,8 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String ){
+    func completeSignIn(id: String, userData: Dictionary<String,String>){
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("Tyson: Data saved to keychain : \(keychainResult)")
         performSegue(withIdentifier: "gotoFeed", sender: nil)
